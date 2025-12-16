27e9414ba670d40c3809723bc4e20c37dd934d62
MilesSpeed22 <lorperdur@gmail.com>
2025-12-16 10:34:43 +0100

    Controller

---
+++ BichoElectrico_Project/Assets/_BichoElectrico_root/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header ("Player Movement")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
