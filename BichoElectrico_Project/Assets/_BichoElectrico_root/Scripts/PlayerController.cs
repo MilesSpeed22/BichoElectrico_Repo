a4bf1222db438a3563eb466f24f4f0a99f792591
MilesSpeed22 <lorperdur@gmail.com>
2025-12-16 12:03:33 +0100

    Movimiento

--- BichoElectrico_Project/Assets/_BichoElectrico_root/Scripts/PlayerController.cs
+++ BichoElectrico_Project/Assets/_BichoElectrico_root/Scripts/PlayerController.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [Header ("Player Movement")]
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
+    [SerializeField] bool isGrounded;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] Transform groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
+
+    Rigidbody2D PlayerRb;
+    PlayerInput input;
+    Vector2 moveInput;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        PlayerRb = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInput>();
+        isGrounded = true;
+    }
+
     void Start()
     {
         
@@ -17,4 +34,36 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    public void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        PlayerRb.linearVelocity = new Vector2(moveInput.x * speed, PlayerRb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        PlayerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+
+    #region Inputs
+
+    public void onMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void onJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded) Jump();
+    }
+
+    #endregion
+
+
 }
