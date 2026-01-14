b72ded89d0b180235b21883d6f053550d3323974
MilesSpeed22 <lorperdur@gmail.com>
2026-01-14 10:04:05 +0100

    muchascosas

---
+++ BichoElectrico_Project/Assets/_BichoElectrico_root/Scripts/Press.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Press : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float delay;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PressMovement()
+    {
+
+    }
+
+}
---
+++ BichoElectrico_Project/Assets/_BichoElectrico_root/Scripts/WindRight.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class WindRight : MonoBehaviour
+{
+    [SerializeField] float windForce = 10f;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerController controller = GetComponent<PlayerController>();
+
+        if (collision.CompareTag("Player"))
+        {
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.AddForce(Vector2.right * windForce * Time.deltaTime, ForceMode2D.Force);
+            }
+        }
+    }
+}
