575b1b212bff7742ac03bb23099810dc5b6d2235
Karslos <cusodiaz1@gmail.com>
2026-01-20 13:11:46 +0100

    Varios
    Arreglo final del mapa tutorial, arreglo de la bala, arreglo del ground checker

--- BichoElectrico_Project/Assets/_BichoElectrico_root/Scripts/Bullet.cs
+++ BichoElectrico_Project/Assets/_BichoElectrico_root/Scripts/Bullet.cs
@@ -20,7 +20,7 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == null || collision.gameObject.tag != "CamConfiner")
+        if (collision.gameObject.tag == null || collision.gameObject.tag != "CamConfiner" && collision.gameObject.tag != "Player")
         {
             gameObject.SetActive(false);
         }
